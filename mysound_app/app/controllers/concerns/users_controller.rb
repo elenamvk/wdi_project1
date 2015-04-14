aef4140d7af00b0d33ee1effea4602e9ae0a066e
elenamvk <elena.sanna.mvk@gmail.com>
2015-04-14 11:50:14 +0100

    created user views, for now only index and show methods are up

---
+++ mysound_app/app/controllers/concerns/users_controller.rb
@@ -0,0 +1,30 @@
+class UsersController < ApplicationController
+
+#before_actions made for admins
+
+before_action :authenticate_user!, except: [:index, :show]
+before_action :set_user, only: [:edit, :update, :destroy]
+
+def index
+  @users = User.all #CREATE VIEWS
+end
+
+def show
+  @user = User.find(params[:id])
+end
+
+#create a provate method that will show the possibilities ONLY to the admin
+#if role == admin then...
+
+private
+  # Use callbacks to share common setup or constraints between actions.
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def user_params
+    params.require(:user).permit(:role, :username, :dob, :about, :icon)
+  end
+
+end
