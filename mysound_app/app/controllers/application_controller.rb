ad95bb147d5b37f85b73457beb5e89a32a1005a9
elenamvk <elena.sanna.mvk@gmail.com>
2015-04-16 10:23:11 +0100

    solved editing issues, i had a double method - duh, elena - said hello in the homepage

--- mysound_app/app/controllers/application_controller.rb
+++ mysound_app/app/controllers/application_controller.rb
@@ -3,19 +3,16 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) do |u| 
       u.permit(:email, :password, :password_confirmation, :username, :dob, :about, :icon)
-    end
-  end 
-
-  def configure_permitted_parameters
+    end 
     devise_parameter_sanitizer.for(:account_update) do |u| 
       u.permit(:email, :password, :password_confirmation, :current_password, :username, :dob, :about, :icon)
     end
   end 
 
-   
+  
 end
